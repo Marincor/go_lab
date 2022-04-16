d141063a66814058fc4bb83c087b54a8b5a8f25d
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2022-04-16 18:31:52 -0300

    feat: 🎸 sites monitoring
    the feature of monitoring sites and return if the site is on or off
    
    BREAKING CHANGE: 🧨 sites.txt included

--- main.go
+++ main.go
@@ -1,7 +1,103 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	showIntro()
+	for {
+		showOptions()
+		command := readCommand()
+
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("here are the logs:")
+		case 0:
+			fmt.Println("bye...")
+			os.Exit(0)
+		default:
+			fmt.Println("invalid option!")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func showOptions() {
+	fmt.Println("1- Start website monitoring")
+	fmt.Println("2- Show logs ")
+	fmt.Println("0- Exit")
+}
+
+func showIntro() {
+	name := "Marincor"
+	github := "@" + name
+	version := "1.0"
+	fmt.Println("Hello,", "for more access the github", github)
+	fmt.Println("This software version is:", version)
+}
+
+func readCommand() (command int) {
+	fmt.Scan(&command)
+	return command
+}
+
+func startMonitoring() {
+	const delay = 5 * time.Minute
+	const monitorings = 5
+	fmt.Println("monitoring...")
+	sites := readSitesTxt()
+
+	for i := 0; i < monitorings; i++ {
+		for _, site := range sites {
+			siteTesting(site)
+		}
+		time.Sleep(delay)
+	}
+
+}
+
+func siteTesting(site string) {
+	res, err := http.Get(site)
+	if err != nil {
+		fmt.Println("something is wrong: ", err)
+		log.Panic(err)
+	}
+
+	switch res.StatusCode {
+	case 200:
+		fmt.Println("Site:", site, "loading with success!")
+	default:
+		fmt.Println("Site", site, "with error, status code:", res.StatusCode)
+	}
+}
+
+func readSitesTxt() []string {
+	var sites []string
+	file, err := os.Open("./sites.txt")
+	if err != nil {
+		fmt.Println("something is wrong: ", err)
+		log.Panic(err)
+	}
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		sites = append(sites, line)
+		if err == io.EOF {
+			break
+		}
+	}
+	file.Close()
+	return sites
 }
